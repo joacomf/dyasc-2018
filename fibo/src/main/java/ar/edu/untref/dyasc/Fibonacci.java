2777c0452f68c1cf028b96ee69bbf44a78a000cb
Joaco <joaquinamf@gmail.com>
2018-08-21 22:54:44 -0300

    Quita líneas con espacios

--- fibo/src/main/java/ar/edu/untref/dyasc/Fibonacci.java
+++ fibo/src/main/java/ar/edu/untref/dyasc/Fibonacci.java
@@ -16,16 +16,16 @@ public class Fibonacci {
         int anterior = 0, actual = 1, proximo;
 
         for (int k = 1; k <= n; ++k){
-            
+
             sucesion.add(anterior);
-            
+
             proximo = anterior + actual;
             anterior = actual;
             actual = proximo;
         }
-        
+
     }
-    
+
     public List<Integer> getSucesion() {
         return sucesion;
     }
--- fibo/src/test/java/ar/edu/untref/dyasc/FibonacciTest.java
+++ fibo/src/test/java/ar/edu/untref/dyasc/FibonacciTest.java
@@ -20,27 +20,27 @@ public class FibonacciTest {
 
     @Test
     public void creaSucesionDeFibonacciCon2Iteraciones() {
-        
+
         Integer[] sucesionEsperada = {0, 1};
         List<Integer> listaEsperada = Arrays.asList(sucesionEsperada);
-        
+
         Fibonacci fibonacci = new Fibonacci();
-        
+
         fibonacci.generarSucesion(2);
-        
+
         Assert.assertEquals(listaEsperada, fibonacci.getSucesion());
     }
-    
+
     @Test
     public void creaSucesionDeFibonacciCon5Iteraciones() {
-        
+
         Integer[] sucesionEsperada = {0, 1, 1, 2, 3};
         List<Integer> listaEsperada = Arrays.asList(sucesionEsperada);
-        
+
         Fibonacci fibonacci = new Fibonacci();
-        
+
         fibonacci.generarSucesion(5);
-        
+
         Assert.assertEquals(listaEsperada, fibonacci.getSucesion());
     }
 }
