f66b198b691b23ca825edb70dd1346ba9ec54c92
Joaco <joaquinamf@gmail.com>
2018-09-08 18:40:06 -0300

    Crea proyecto Librería

---
+++ libreria/src/main/java/ar/edu/untref/dyasc/Ejecutable.java
@@ -0,0 +1,11 @@
+package ar.edu.untref.dyasc;
+
+public class Ejecutable {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hola a todos!!!");
+
+    }
+
+}
---
+++ libreria/src/test/java/ar/edu/untref/dyasc/LibreriaTest.java
@@ -0,0 +1,12 @@
+package ar.edu.untref.dyasc;
+
+import org.junit.Test;
+
+public class LibreriaTest {
+
+    @Test
+    public void existeLibreria() {
+        new Libreria();
+    }
+
+}
