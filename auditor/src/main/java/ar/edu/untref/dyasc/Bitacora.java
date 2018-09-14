8413626ed8ca39a94d31663e1d5f49f51df40881
Joaco <joaquinamf@gmail.com>
2018-09-14 19:01:38 -0300

    Agrega impresión en archivo e ignora el mismo

--- auditor/src/main/java/ar/edu/untref/dyasc/Bitacora.java
+++ auditor/src/main/java/ar/edu/untref/dyasc/Bitacora.java
@@ -1,5 +1,10 @@
 package ar.edu.untref.dyasc;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
@@ -12,7 +17,21 @@ public class Bitacora {
 
         String textoARegistrar = fechaFormateada + " - " + mensaje;
 
+        agregarLineaEnArchivoDeBitacora(textoARegistrar);
         System.out.println(textoARegistrar);
     }
 
+    private static void agregarLineaEnArchivoDeBitacora(String textoAImprimir) {
+        File archivo = new File("bitacora.txt");
+
+        try {
+            Writer escritor = new FileWriter(archivo, true);
+            BufferedWriter buffer = new BufferedWriter(escritor);
+            buffer.append(textoAImprimir);
+            buffer.append("\n");
+            buffer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
