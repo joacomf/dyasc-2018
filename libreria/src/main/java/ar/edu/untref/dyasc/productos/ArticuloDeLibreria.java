3c92e5ce6f6e02e27e690217bc98be57bb53a425
Joaco <joaquinamf@gmail.com>
2018-09-27 00:46:24 -0300

    Extrae iva a atributo

--- libreria/src/main/java/ar/edu/untref/dyasc/productos/ArticuloDeLibreria.java
+++ libreria/src/main/java/ar/edu/untref/dyasc/productos/ArticuloDeLibreria.java
@@ -2,12 +2,14 @@ package ar.edu.untref.dyasc.productos;
 
 public class ArticuloDeLibreria extends Producto {
 
+    private double iva = 1.21;
+
     public ArticuloDeLibreria(double precioDeVenta) {
         super(precioDeVenta);
     }
 
     @Override
     public double obtenerPrecio() {
-        return super.obtenerPrecio() * 1.21;
+        return super.obtenerPrecio() * iva;
     }
 }
