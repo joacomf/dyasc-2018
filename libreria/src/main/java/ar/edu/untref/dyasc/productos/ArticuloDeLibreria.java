d28f02d3b57e0918b874598d340ee3c39680da5c
Joaco <joaquinamf@gmail.com>
2018-09-27 00:48:52 -0300

    Cambia iva a variable loca

--- libreria/src/main/java/ar/edu/untref/dyasc/productos/ArticuloDeLibreria.java
+++ libreria/src/main/java/ar/edu/untref/dyasc/productos/ArticuloDeLibreria.java
@@ -2,14 +2,13 @@ package ar.edu.untref.dyasc.productos;
 
 public class ArticuloDeLibreria extends Producto {
 
-    private double iva = 1.21;
-
     public ArticuloDeLibreria(double precioDeVenta) {
         super(precioDeVenta);
     }
 
     @Override
     public double obtenerPrecio() {
+        double iva = 1.21;
         return super.obtenerPrecio() * iva;
     }
 }
