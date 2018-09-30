1bb8204b99840e0f99400ec040cde3bd12f3cae0
Joaquín Moreno Fernández <joaquin.moreno@grupoesfera.com.ar>
2018-09-29 21:30:58 -0300

    Agrega manejo de errores con la variable de entorno

--- auditor/src/main/java/ar/edu/untref/dyasc/impresion/AdministradorDeImpresoras.java
+++ auditor/src/main/java/ar/edu/untref/dyasc/impresion/AdministradorDeImpresoras.java
@@ -9,13 +9,19 @@ public class AdministradorDeImpresoras implements Imprimible{
     private List<Imprimible> impresoras = new LinkedList<>();
 
     public AdministradorDeImpresoras(String textoDeOpciones){
+        if (textoDeOpciones == null) throw new OpcionInvalidaException("No se definió la variable bitacora.destino");
+
         String[] opciones = textoDeOpciones.split(",");
+
+        if (opciones.length == 0) throw new OpcionInvalidaException("No hay opciones disponibles para generar el Log");
 
         Arrays.asList(opciones).forEach(this::crearPorTipo);
     }
 
     private void crearPorTipo(String opcion) {
         Imprimible impresora;
+
+        if (opcion.equals("")) throw new OpcionInvalidaException("Hay una opción vacía");
 
         if (!opcion.equals("CONSOLA")) {
             impresora = new ImpresoraDeArchivo(opcion);
