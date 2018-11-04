b771224587df62a9c6b69e577e9b3fc5832b871b
Joaquín Moreno Fernández <joaquin.moreno@grupoesfera.com.ar>
2018-11-04 17:54:27 -0300

    Agrega test a tablero

--- batalla_naval/src/test/java/ar/edu/untref/dyasc/TableroTest.java
+++ batalla_naval/src/test/java/ar/edu/untref/dyasc/TableroTest.java
@@ -1,5 +1,21 @@
 package ar.edu.untref.dyasc;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class TableroTest {
+
+    @Test
+    public void tableroDe5por5PermiteColocarBoteEnPosicion1y1(){
+
+        Bote bote = new Bote();
+        Tablero tablero = new Tablero(5, 5);
+
+        tablero.colocar(bote, 1, 1);
+
+        Barco boteEnTablero = tablero.obtener(1, 1);
+
+        Assert.assertEquals(bote, boteEnTablero);
+    }
 
 }
